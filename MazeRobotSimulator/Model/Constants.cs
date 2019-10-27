a3c824d6f6bdbb6459a61c5f2c2b13f8115b9412
prbasha <pbasha82@gmail.com>
2019-10-27 16:46:33 +1100

    Updates to maze size and simulation speed
    Changed the maximim maze size to 100x100.
    Changed the maximum simulation speed to 100x.

--- MazeRobotSimulator/Model/Constants.cs
+++ MazeRobotSimulator/Model/Constants.cs
@@ -6,10 +6,10 @@
     public static class Constants
     {
         public const int MinimumMazeWidthHeightCells = 20;
-        public const int MaximumMazeWidthHeightCells = 50;
+        public const int MaximumMazeWidthHeightCells = 100;
         public const int DefaultMazeWidthHeightCells = 30;
         public const int MinimumSimulationSpeed = 1;
-        public const int MaximumSimulationSpeed = 20;
+        public const int MaximumSimulationSpeed = 100;
         public const int DefaultSimulationSpeed = 1;
         public const int DefaultStepIntervalMilliSeconds = 1000;
         public const int MazeGenerationDelayMilliSeconds = 1;
